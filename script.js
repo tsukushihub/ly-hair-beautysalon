d4c63259f2a8e03c2172e53a3411f232054b98d2
tsukushihub <tsukushihub@example.com>
2026-05-02 22:36:36 +0900

    商用利用のための機能追加と画像パスの修正

--- script.js
+++ script.js
@@ -32,7 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
     
     if (burger) {
         burger.addEventListener('click', () => {
+            // Toggle Nav
             nav.classList.toggle('nav-active');
+
+            // Animate Links
+            const navLinks = document.querySelectorAll('.nav-links li');
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+            // Burger Animation
             burger.classList.toggle('toggle');
         });
     }
